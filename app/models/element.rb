22ae1af8ae70175f276355a84e4b03c05eb5879a
josuekabangu <ajkabs2@gmail.com>
2021-08-03 22:35:54 +0200

    scaffold element & setup

--- app/models/element.rb
+++ app/models/element.rb
@@ -1,4 +1,18 @@
 class Element < ApplicationRecord
   belongs_to :post
   has_rich_text :content
+  has_one_attached :image
+  has_one_attached :element_video
+
+  def paragraph?
+    element_type == "paragraph"
+  end
+
+  def image?
+    element_type == "image"
+  end
+
+  def element_video?
+    element_type == "element_video"
+  end
 end
